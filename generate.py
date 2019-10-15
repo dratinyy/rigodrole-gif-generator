0a65b89b550113db8f988b4bdcec5bb13f0cd692
Elliot Kauffmann <elliot.kauffmann@etu.univ-cotedazur.fr>
2019-10-15 20:06:06 +0200

    Et voilà l'travail

--- generate.py
+++ generate.py
@@ -9,62 +9,97 @@ import imageio
 # 3. Compile à nouveau le gif
 
 background_image_name   = "background/bg9.gif"
-frame_count_multiplier  = 1
-frame_duration          = 0.1
-photo_name              = "photo.png"
+frame_count_multiplier  = 2
+frame_duration          = 0.04
+photo_name              = "Hugu.png"
 item_names              = [ "items/bombe.png",
-                            "items/placard.png",
                             "items/canard.png",
                             "items/cochon.png",
+                            "items/placard.png",
                             "items/tomate.png" ]
 
 item_data               = [ { "item_index": 0,
-                              "size": (80, 80),
-                              "position": (260, 200),
-                              "initial_angle": 120,
-                              "rotation_speed": -0.5,
+                              "size": (110, 110),
+                              "position": (530, 20),
+                              "initial_angle": 0,
+                              "rotation_speed": 1,
                               "rotation_direction": 1 },
 
-                              { "item_index": 2,
-                              "size": (80, 80),
-                              "position": (20, 50),
-                              "initial_angle": 160,
-                              "rotation_speed": 1,
+                              { "item_index": 0,
+                              "size": (110, 110),
+                              "position": (270, 530),
+                              "initial_angle": 75,
+                              "rotation_speed": 3,
                               "rotation_direction": -1 },
 
-                              { "item_index": 3,
-                              "size": (80, 80),
-                              "position": (90, 250),
-                              "initial_angle": 240,
-                              "rotation_speed": -1,
+                              { "item_index": 0,
+                              "size": (110, 110),
+                              "position": (10, 520),
+                              "initial_angle": 210,
+                              "rotation_speed": 4,
+                              "rotation_direction": 1 },
+
+                              { "item_index": 0,
+                              "size": (110, 110),
+                              "position": (10, 200),
+                              "initial_angle": 260,
+                              "rotation_speed": 7,
                               "rotation_direction": -1 },
 
                               { "item_index": 1,
-                              "size": (80, 80),
-                              "position": (120, 10),
+                              "size": (150, 150),
+                              "position": (490, 170),
                               "initial_angle": 30,
+                              "rotation_speed": 3,
+                              "rotation_direction": -1 },
+
+                              { "item_index": 1,
+                              "size": (150, 150),
+                              "position": (310, 130),
+                              "initial_angle": 270,
+                              "rotation_speed": 4,
+                              "rotation_direction": 1 },
+
+                              { "item_index": 1,
+                              "size": (150, 150),
+                              "position": (200, 320),
+                              "initial_angle": 120,
+                              "rotation_speed": 6,
+                              "rotation_direction": -1 },
+
+                              { "item_index": 2,
+                              "size": (160, 160),
+                              "position": (100, 450),
+                              "initial_angle": 210,
+                              "rotation_speed": 4,
+                              "rotation_direction": 1 },
+
+                              { "item_index": 3,
+                              "size": (250, 250),
+                              "position": (110, 30),
+                              "initial_angle": 210,
                               "rotation_speed": 1,
                               "rotation_direction": 1 },
 
                               { "item_index": 4,
                               "size": (80, 80),
-                              "position": (420, 310),
-                              "initial_angle": 60,
-                              "rotation_speed": -0.7,
-                              "rotation_direction": 1 },
-
-                              { "item_index": 3,
-                              "size": (80, 80),
-                              "position": (280, 70),
-                              "initial_angle": 120,
-                              "rotation_speed": 1,
+                              "position": (50, 60),
+                              "initial_angle": 310,
+                              "rotation_speed": 8,
                               "rotation_direction": -1 },
 
-                              { "item_index": 1,
+                              { "item_index": 4,
                               "size": (80, 80),
-                              "position": (5, 160),
-                              "initial_angle": 210,
-                              "rotation_speed": 1,
+                              "position": (60, 350),
+                              "initial_angle": 220,
+                              "rotation_speed": 3,
+                              "rotation_direction": -1 },
+
+                              { "item_index": 4,
+                              "size": (80, 80),
+                              "position": (380, 20),
+                              "initial_angle": 70,
+                              "rotation_speed": 4,
                               "rotation_direction": 1 } ]
 
 # Ouvre le gif de fond
